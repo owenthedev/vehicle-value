cf12cceda5ccfed5f7982e4c5ac62c02bf534203
owenthedev <86202551+owenthedev@users.noreply.github.com>
2022-05-01 02:19:33 +0200

    Update vehiclevalue.py

--- vehiclevalue.py
+++ vehiclevalue.py
@@ -5,14 +5,6 @@ Created on Sat Apr 30 22:11:42 2022
 @author: owen nxumalo, carl nxumalo
 """
 
-
-
-"""
-Created on Sat Apr 30 22:11:42 2022
-
-@author: owenn
-"""
-
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
