a705e91bebf81f915e510cad1d6f711f6e6b9baa
owenthedev <86202551+owenthedev@users.noreply.github.com>
2022-05-03 02:06:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -214,11 +214,11 @@ with col1:
 
 with col2:
     st.title('VEHICLE VALUE CHECKER')
-    st.write('Get a good idea about the value of your car from us, an unbiased source.')
+    st.subheader('Get a good idea about the value of your car from us, an unbiased source.')
    
 
 
-st.write('')
+
 st.write('Step 1- Go to cars.co.za and search for your car there (select make, model year, mileage etc).')
 st.write('Step 2- Copy the url and paste it into the text field.')
 st.write('Need help? Watch the explainer video below')
