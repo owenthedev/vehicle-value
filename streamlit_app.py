54f34ec86ec810ab9e077c05ead167870ad9675e
owenthedev <86202551+owenthedev@users.noreply.github.com>
2022-05-03 02:04:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -222,7 +222,6 @@ st.write('')
 st.write('Step 1- Go to cars.co.za and search for your car there (select make, model year, mileage etc).')
 st.write('Step 2- Copy the url and paste it into the text field.')
 st.write('Need help? Scroll down to watch the explainer video below')
-st.write('')
 
 url_in = st.text_input(label='Paste the URL here then press Enter',value='')
 if url_in=='':
