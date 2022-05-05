03f0b14d32588599b116edd419a0c62f290aee8d
CarlNxumalo <104698686+CarlNxumalo@users.noreply.github.com>
2022-05-05 16:56:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -160,65 +160,8 @@ def get_price(url):
     plt.title('Plot of Vehicle Mileage and Price')
     # showing legend
     plt.legend()
-    plt.savefig('chart.jpeg')
+    plt.savefig('chart.png')
     
-
-    def estimate_coef(x, y):
-        # number of observations/points
-        n = np.size(x)
-
-        # mean of x and y vector
-        m_x = np.mean(x)
-        m_y = np.mean(y)
-
-        # calculating cross-deviation and deviation about x
-        SS_xy = np.sum(y*x) - n*m_y*m_x
-        SS_xx = np.sum(x*x) - n*m_x*m_x
-
-        # calculating regression coefficients
-        b_1 = SS_xy / SS_xx
-        b_0 = m_y - b_1*m_x
-
-        return (b_0, b_1)
-
-    def plot_regression_line(x, y, b):
-        # plotting the actual points as scatter plot
-        plt.scatter(x, y, color = "m",
-                marker = ".", s = 14)
-
-        # predicted response vector
-        y_pred = b[0] + b[1]*x
-
-        # plotting the regression line
-        plt.plot(x, y_pred, color = "g")
-
-        # putting labels
-        plt.xlabel('x')
-        plt.ylabel('y')
-
-        # function to show plot
-        plt.show()
-
-    def regg():
-        # observations / data
-        x = np.array(mileageNum)
-        y = np.array(pricesaNum)
-
-        # estimating coefficients
-        b = estimate_coef(x, y)
-        print("Estimated coefficients:\nb_0 = {} \
-            \nb_1 = {}".format(b[0], b[1]))
-
-      #enter your mileage 
-
-        # plotting regression line
-        plot_regression_line(x, y, b)
-
-    if __name__ == "__regg__":
-        regg()
-    print(r)
-    def myfunc(x):
-      return slope * x + intercept
 
     
     
